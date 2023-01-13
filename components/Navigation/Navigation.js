ae25c9013a6314b23ddab622e7809a7cefc4a4ed
Muhammad Talha <muhammad.talha.776932@gmail.com>
2023-01-13 15:41:26 +0500

    Successful add the Bottom Tabs

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { View } from 'react-native'
 import {NavigationContainer} from "@react-navigation/native"
-import { createStackNavigator } from "@react-navigation/stack"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import Home from "../Screens/Home"
 import About from "../Screens/About"
-const stack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
 
 export default function Navigation() {
 
   return (
    <NavigationContainer >
-    <stack.Navigator >
-        <stack.Screen name='Home' component={Home} />
-        <stack.Screen name='About' component={About}/>
-    </stack.Navigator>
+    <Tabs.Navigator >
+        <Tabs.Screen name='Home' component={Home} />
+        <Tabs.Screen name='About' component={About}/>
+    </Tabs.Navigator>
    </NavigationContainer> 
   )
 }
--- components/Screens/Home.js
+++ components/Screens/Home.js
@@ -1,20 +1,11 @@
-import React, { useState }  from 'react';
-import { Text,SafeAreaView,Pressable, View  } from 'react-native'
+import React  from 'react';
+import { Text,SafeAreaView, View  } from 'react-native'
 
 function Home({navigation}) {
-    const goToAbout = () =>{
-        navigation.navigate("About")
-    }
   return (
     <SafeAreaView className = "flex-1 justify-center items-center">
         <View>
             <Text className="text-3xl font-serif font-normal">Home Screen</Text>
-            <Pressable
-            className = "bg-slate-500"
-            onPress={goToAbout}
-            >
-                <Text className="text-center font-bold text-4xl">Go to About</Text>
-            </Pressable>
         </View>
     </SafeAreaView>
   )
