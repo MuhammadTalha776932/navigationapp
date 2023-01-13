2f259d36d2730cd3482556d9f60f14fbb0861891
Muhammad Talha <muhammad.talha.776932@gmail.com>
2023-01-13 12:42:46 +0500

    Updates the App.js
    Improved the UI

--- App.js
+++ App.js
@@ -17,7 +17,8 @@ import {
   Text,
   useColorScheme,
   View,
-  TextInput, TouchableOpacity
+  TextInput, TouchableOpacity,Pressable
+  
 } from 'react-native';
 
 
@@ -27,32 +28,42 @@ const App = () => {
   const [password, setPassword] = useState('');
 
   const handleSubmit = () => {
-    
+    alert("Hello!");
   };
   return (
     <SafeAreaView className="flex-1 flex-col h-full">
-      <View className="flex-[1] items-center justify-center content-center bottom-[25%]  bg-teal-600">
+      <View className="relative h-[200px] items-center justify-center content-center bg-teal-600
+      ">
         <Text className="text-gray-700 font-serif text-2xl">{`Welcome ${email || "to React Native"}`}</Text>
       </View>
-      <View className="top-[-50%] w-full flex-initial justify-center">
-        <View className="bg-white p-6 rounded-lg shadow-md">
-        <Text className="text-xl  mb-2 text-center mt-4 font-sans">Sign In</Text>
+      <View className="flex-initial bottom-2 justify-start h-[500px]">
+        <View className="flex justify-center bg-white h-full p-6 rounded-lg shadow-2xl
+       
+        ">
+
+        <Text className="text-2xl mb-5 bottom-4 text-center mt-4 font-sans uppercase">Login</Text>
+        
         <TextInput
-          className="bg-gray-200 p-2 rounded-lg mb-3"
+          className="bg-gray-200 p-2 text-2xl rounded-lg h-[50px] mb-6"
           placeholder="Email"
+          keyboardType="email-address"
           onChangeText={setEmail}
           value={email}
         />
         <TextInput
-          className="bg-gray-200 p-2 rounded-lg mb-3"
+          className="bg-gray-200 p-2 text-2xl rounded-lg h-[50px] mb-6"
           placeholder="Password"
           onChangeText={setPassword}
           value={password}
           secureTextEntry={true}
         />
-        <TouchableOpacity className="bg-indigo-500 text-white p-2 rounded-lg" onPress={handleSubmit}>
-          <Text>Sign In</Text>
+        <View className="text-center items-center hover:bg-black">
+        <TouchableOpacity className="bg-indigo-500 w-[300px] text-white p-2 rounded-lg
+        hover:text-black
+        " activeOpacity={0.9} onPress={handleSubmit}>
+          <Text className="text-lg text-center text-white">Sign In</Text>
         </TouchableOpacity>
+        </View>
         </View>
       </View>
     </SafeAreaView>
