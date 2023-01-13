f47bb7204dc12107db1b40cdb9abe57d80a138a0
Muhammad Talha <muhammad.talha.776932@gmail.com>
2023-01-13 17:11:09 +0500

    successful implement the icon tab navigation

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -4,15 +4,29 @@ import {NavigationContainer} from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import Home from "../Screens/Home"
 import About from "../Screens/About"
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5"
 const Tabs = createBottomTabNavigator();
 
 export default function Navigation() {
 
   return (
    <NavigationContainer>
-      <Tabs.Navigator screenOptions={ ({route}) =>({tabBarIcon:({focused,color,size})=>({
-        
-      })})}>
+      <Tabs.Navigator screenOptions={ ({route}) =>({tabBarIcon:({focused,color,size})=>{
+        let iconName;
+        if(route.name === "Home"){
+          iconName="home";
+          size= focused? 25 : 20;
+          color=focused? "green": "black";
+         
+        }else{
+          iconName="info";
+          size= focused? 25 : 20;
+          color=focused? "green": "black";
+        }
+        return(
+          <FontAwesome5 name={iconName} size={size} color={color}/>
+        )
+      }})}>
           <Tabs.Screen  name='Home' component={Home} />
           <Tabs.Screen name='About' component={About}/>
       </Tabs.Navigator>
