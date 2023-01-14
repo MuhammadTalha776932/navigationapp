3dc8613aaedeb5ca0c7331c7bb46c4a03efa098b
Muhammad Talha <muhammad.talha.776932@gmail.com>
2023-01-14 15:01:32 +0500

    Added the Drawer.Navigation feature....

---
+++ components/Drawer/Drawer.navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const DrawerNavigation = () => (
+    <View>
+        <Text>DrawerNavigation</Text>
+    </View>
+);
+
+export default DrawerNavigation;
