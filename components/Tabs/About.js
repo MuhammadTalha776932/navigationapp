8ec8249e190aa40efa43bffd4aa0de4f8d68815d
Muhammad Talha <muhammad.talha.776932@gmail.com>
2023-01-18 13:03:37 +0500

    modified the About section...

--- components/Tabs/About.js
+++ components/Tabs/About.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text,View,ScrollView } from 'react-native';
 
 
 const About = ({
     params,
 }) => (
     <>
-        <Text>About</Text>
+        <ScrollView style={{flex:1,backgroundColor:"gray"}}>
+            <View style={{flex:1,height:300,justifyContent:"center",alignItems:"center"}}>
+                <Text>Section One</Text>
+            </View>
+            <View style={{flex:1,height:300,justifyContent:"center",alignItems:"center"}}>
+                <Text>Section Two</Text>
+            </View>
+            <View style={{flex:1,height:300,justifyContent:"center",alignItems:"center"}}>
+                <Text>Section Three</Text>
+            </View>
+            <View style={{flex:1,height:300,justifyContent:"center",alignItems:"center"}}>
+                <Text>Section Four</Text>
+            </View>
+        </ScrollView>
     </>
 );
 
